ba882ab9f4ceee7a467c244456c9f68bd90d6f26
MumtahinaRahman <vanallope2015@gmail.com>
2021-10-13 14:44:24 +0600

    updated madlib passages

---
+++ hp.py
@@ -0,0 +1,29 @@
+# this file will contain a passage from harry potter books
+
+def madlib() :
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    noun6 = input("Noun: ")
+    noun_plural = input("Noun plural: ")
+    body_part = input("Body part: ")
+    body_part2 = input("Body part: ")
+    verb = input("Verb: ")
+    verb_past = input("Verb (past tense): ")
+    verb_past2 = input("Verb (past tense): ")
+
+    madlib = f"the bang was like a {noun1}, and the {adj2} {noun2} that erupted between them, \
+at the dead center of the circle they had been {noun3}, marked the point where the \
+{noun_plural} {verb_past}. Harry saw Voldemort’s {adj3} jet meet his own spell, saw the Elder Wand \
+fly high, spinning through the air toward the master it would not {verb},  And Harry, with the {adj1} skill of a Seeker, \
+caught the {noun4} in his free {body_part} as Voldemort fell backward, arms splayed, the slit pupils \
+of the {adj4} {body_part2} rolling upward. Voldemort was dead, {verb_past2} by his own rebounding {noun5}, \
+and Harry stood staring down at his enemy’s {noun6}."
+
+    print(madlib)
--- madlib.py
+++ madlib.py
@@ -1,2 +1,12 @@
 # hullooooo!!!
 # let's play madlib
+
+import hp, hg, zombie
+import random
+
+passages = [hg.madlib(), zombie.madlib(), hp.madlib()]
+random.choice(passages)
+
+
+
+
